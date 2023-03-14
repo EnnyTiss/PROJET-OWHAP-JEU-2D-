f26c6bbcaebb2da9f649a3401a1d20c7f84d2c1d
JAMALI OUMAYMA p1810913 <oumayma.jamali@etu.univ-lyon1.fr>
2023-03-14 13:44:53 +0000

    Upload New File

---
+++ src/Timer.h
@@ -0,0 +1,29 @@
+#ifndef _COMPTEREBOURS_H
+#define _COMPTEREBOURS_H
+
+#include "cassert"
+
+class Timer {
+    private:
+
+        int val;
+        int valMin;
+        int valMax;
+        bool actifCompteur;
+
+    public:
+
+        Timer();
+        Timer(int val, int valMin, int valMax, bool actifCompteur);
+//+ incrementer(): bool
+//+ decrementer(): bool
+        void reinitialiser();
+        void setVal(int v);
+        void setActif(bool b);
+        int getValMin() const;
+        int getValMax() const;
+        void GameOver();
+        int ecoulementTimer();
+};
+
+#endif
