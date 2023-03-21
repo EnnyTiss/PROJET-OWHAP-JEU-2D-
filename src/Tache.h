d7a43980114f559e82ecc6397a33742a9d9224a3
JAMALI OUMAYMA p1810913 <oumayma.jamali@etu.univ-lyon1.fr>
2023-03-21 07:26:34 +0000

    Replace Tache.h

--- src/Tache.h
+++ src/Tache.h
@@ -5,17 +5,27 @@ class Tache {
       
     private:
     int idTache;
+    int difficulte;
+
+    void Combinaison(); //saisie combinaison de touches
+    void RecopierCode(); //saisir le code affiché à l'écran
+    void Calcul(); //résoudre un calcul simple
+    void SpamTouche(); //appuyer sur une touche un nombre de fois
+    void Question(); //répondre à une question 
+    void RecopierMot(); //saisir le mot affiché à l'écran
+    void AppuiTouche(); //appuyer sur une touche 
 
     public:
 //constructeurs/destructeur
     Tache();
-    Tache(int idT);
+    Tache(int idT, int difficulte);
     ~Tache();
 //accesseurs
     int getIdTache() const;
 //actions sur la tache
-    void actionTache();
-      
+    void actionTache(int idT);
+
+     
 };
 
 inline int Tache::getIdTache () const { return idTache; }
